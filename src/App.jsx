37cddcb6579ca29fc359ee7d93ca55431c73d372
RDJ ironman <ironman053103@gmail.com>
2024-01-27 15:31:46 +0530

    Done with the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-
-import './App.css';
-import Student from "./components/Registration_forms/student"
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './components/layout';
+import Home from './components/home';
+import Student from './components/Registration_forms/student';
 import University from './components/Registration_forms/university';
 import OParty from './components/Registration_forms/OParty';
-import Layout from './components/layout';
 
 function App() {
   return (
     <div className="App">
-      {/* <Student/>
-      <OParty/>
-      <University/> */}
-      <Layout/>
+      <Router>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/reg_stu" element={<Student />} />
+            <Route path="/reg_uni" element={<University />} />
+            <Route path="/reg_Opa" element={<OParty />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,23 +1,31 @@
-
 import React, { useState } from 'react';
 import {
-    MDBNavbar,
-      MDBContainer,
-      MDBNavbarBrand,
-      MDBNavbarItem,
-      MDBNavbarLink,
-      MDBDropdown,
-      MDBDropdownItem,
-      MDBDropdownMenu,
-      MDBDropdownToggle,
-      MDBNavbarNav,
-      MDBNavbarToggler,
-      MDBIcon,
-      MDBCollapse
+  MDBNavbar,
+  MDBContainer,
+  MDBNavbarBrand,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBDropdown,
+  MDBDropdownItem,
+  MDBDropdownMenu,
+  MDBDropdownToggle,
+  MDBNavbarNav,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBCollapse,
+  MDBCol,
+  MDBRow,
+  MDBBtn,
+  MDBInput,
+  MDBFooter
 } from 'mdb-react-ui-kit';
-
-export default function App() {
+import { useNavigate,Outlet } from 'react-router-dom';
+export default function Layout() {
   const [openNavNoTogglerSecond, setOpenNavNoTogglerSecond] = useState(false);
+  const handleInnerDropdownClick = (e) => {
+    e.stopPropagation();
+  };
+  const navigate=useNavigate();
 
   return (
     <>
@@ -30,7 +38,7 @@ export default function App() {
             aria-controls='navbarTogglerDemo02'
             aria-expanded='false'
             aria-label='Toggle navigation'
-            
+
             onClick={() => setOpenNavNoTogglerSecond(!openNavNoTogglerSecond)}
           >
             <MDBIcon icon='bars' fas />
@@ -52,18 +60,211 @@ export default function App() {
               </MDBNavbarItem>
             </MDBNavbarNav>
             <MDBDropdown>
-                <MDBDropdownToggle tag='a' className='nav-link' style={{ color: '#fff', cursor: 'pointer',fontSize: '1.5rem' }}>
+              <MDBDropdownToggle tag='a' className='nav-link' style={{ color: '#fff', cursor: 'pointer', fontSize: '1.5rem' }}>
                 <MDBIcon icon='bars' fas />
-                </MDBDropdownToggle>
-                <MDBDropdownMenu >
-                  <MDBDropdownItem  link>Login</MDBDropdownItem>
-                  <MDBDropdownItem link>Register</MDBDropdownItem>
- 
-                </MDBDropdownMenu>
-              </MDBDropdown>
+              </MDBDropdownToggle>
+              <MDBDropdownMenu >
+                <MDBDropdownItem link>Login</MDBDropdownItem>
+                <MDBDropdownItem link >
+                  <MDBDropdown onClick={handleInnerDropdownClick}>
+                    <MDBDropdownToggle tag="a" className='nav-link' >
+                      Register
+                    </MDBDropdownToggle>
+                    <MDBDropdownMenu>
+                      <MDBDropdownItem link onClick={() => navigate("/reg_stu")}>Student</MDBDropdownItem>
+                      <MDBDropdownItem link onClick={() => navigate("/reg_uni")}>University</MDBDropdownItem>
+                      <MDBDropdownItem link onClick={() => navigate("/reg_Opa")}>Other Party</MDBDropdownItem>
+
+                    </MDBDropdownMenu>
+                  </MDBDropdown>
+                </MDBDropdownItem>
+
+              </MDBDropdownMenu>
+            </MDBDropdown>
           </MDBCollapse>
         </MDBContainer>
       </MDBNavbar>
+      <main>
+        <Outlet/>
+      </main>
+      <MDBFooter className='text-center ' color='white' bgColor='dark'>
+      <MDBContainer className='p-4'>
+        <section className='mb-4'>
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='facebook-f' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='twitter' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='google' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='instagram' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='linkedin-in' />
+          </MDBBtn>
+
+          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+            <MDBIcon fab icon='github' />
+          </MDBBtn>
+        </section>
+
+        <section className=''>
+          <form action=''>
+            <MDBRow className='d-flex justify-content-center'>
+              <MDBCol size="auto">
+                <p className='pt-2'>
+                  <strong>Sign up for our newsletter</strong>
+                </p>
+              </MDBCol>
+
+              <MDBCol md='5' start>
+                <MDBInput contrast type='email' label='Email address' className='mb-4' />
+              </MDBCol>
+
+              <MDBCol size="auto">
+                <MDBBtn outline color='light' type='submit' className='mb-4'>
+                  Subscribe
+                </MDBBtn>
+              </MDBCol>
+            </MDBRow>
+          </form>
+        </section>
+
+        {/* <section className='mb-4'>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum repellat quaerat
+            voluptatibus placeat nam, commodi optio pariatur est quia magnam eum harum corrupti dicta, aliquam
+            sequi voluptate quas.
+          </p>
+        </section>
+
+        <section className=''>
+          <MDBRow>
+            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+              <h5 className='text-uppercase'>Links</h5>
+
+              <ul className='list-unstyled mb-0'>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 4
+                  </a>
+                </li>
+              </ul>
+            </MDBCol>
+
+            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+              <h5 className='text-uppercase'>Links</h5>
+
+              <ul className='list-unstyled mb-0'>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 4
+                  </a>
+                </li>
+              </ul>
+            </MDBCol>
+
+            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+              <h5 className='text-uppercase'>Links</h5>
+
+              <ul className='list-unstyled mb-0'>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 4
+                  </a>
+                </li>
+              </ul>
+            </MDBCol>
+
+            <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
+              <h5 className='text-uppercase'>Links</h5>
+
+              <ul className='list-unstyled mb-0'>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 2
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 3
+                  </a>
+                </li>
+                <li>
+                  <a href='#!' className='text-white'>
+                    Link 4
+                  </a>
+                </li>
+              </ul>
+            </MDBCol>
+          </MDBRow>
+        </section> */}
+      </MDBContainer>
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2020 Copyright: 
+        <a className='text-white' href='https://mdbootstrap.com/'>
+          Vamshi_Mike_Abhiram
+        </a>
+      </div>
+    </MDBFooter>
     </>
   );
 }
